8158f2acd5ae5d5e45d38842f4a4c43555d76709
1NEA01711\bernasconib <brunobernascon@gmail.com>
2018-02-21 02:21:44 -0300

    ...

--- dash/ini.php
+++ dash/ini.php
@@ -337,7 +337,7 @@
     </section>
 
     <!-- Main content -->
-    <section class="content">
+    <!--<section class="content">
       <!-- Small boxes (Stat box) -->
       <div class="row">
         <div class="col-lg-3 col-xs-6">
@@ -836,7 +836,7 @@
         </section>
         <!-- right col -->
       </div>
-      <!-- /.row (main row) -->
+      <!-- /.row (main row) --> -->
 
     </section>
     <!-- /.content -->
